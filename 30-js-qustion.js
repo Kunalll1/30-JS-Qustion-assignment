5504763968242919993f0cd889dbbb93754b3d39
kunalll1 <kunalkumarjha97@gmail.com>
2023-01-23 18:39:49 +0530

    solved the 1st qustion

---
+++ 30-js-qustion.js
@@ -0,0 +1,8 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+let variableString = "30 JS Questions";
+// console.log(variableString); OUTPUT:::: [30 JS Questions]
+let variableBoolean = false;
+// console.log(variableBoolean); OUTPUT:::: [false]
+let emptyVariable;
+// console.log(emptyVariable); OUTPUT:::: [undefined]
