bee7a39233d35441c2a462c5badc855ef2e0cbbc
kunalll1 <kunalkumarjha97@gmail.com>
2023-01-24 19:26:29 +0530

    solved 2nd qustion

--- 30-js-qustion.js
+++ 30-js-qustion.js
@@ -6,3 +6,16 @@ let variableBoolean = false;
 // console.log(variableBoolean); OUTPUT:::: [false]
 let emptyVariable;
 // console.log(emptyVariable); OUTPUT:::: [undefined]
+
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+let firstName = "Kunal";
+// console.log(firstName); OUTPUT:::: [Kunal]
+let lastName = "Kumar";
+// console.log(lastName); OUTPUT:::: [Kumar]
+let maritalStatus = "non-married";
+// console.log(maritalStatus); OUTPUT:::: [non-married]
+let age = 21;
+// console.log(age);OUTPUT:::: [21]
+console.log(
+  `my name is ${firstName} ${lastName}, my age is ${age} year's and ${maritalStatus}`
+);
