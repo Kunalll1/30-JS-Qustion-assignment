695119724cff34bd6f1e1b97322ea752a6552607
kunalll1 <kunalkumarjha97@gmail.com>
2023-01-24 19:33:50 +0530

    solved 3nd qustion

--- 30-js-qustion.js
+++ 30-js-qustion.js
@@ -16,6 +16,12 @@ let maritalStatus = "non-married";
 // console.log(maritalStatus); OUTPUT:::: [non-married]
 let age = 21;
 // console.log(age);OUTPUT:::: [21]
-console.log(
-  `my name is ${firstName} ${lastName}, my age is ${age} year's and ${maritalStatus}`
-);
+// console.log(
+//   `my name is ${firstName} ${lastName}, my age is ${age} year's and ${maritalStatus}`
+// );
+
+// 03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+let changeUpperCase =
+  "My name is Kunal, and I am currently learning Full Stack Web Development under the awesome teachers Hetash Sir and Anurage Sir. I love the way they explain the topics.";
+
+// console.log(changeUpperCase.toUpperCase()); OUTPUT:::: [MY NAME IS KUNAL, AND I AM CURRENTLY LEARNING FULL STACK WEB DEVELOPMENT UNDER THE AWESOME TEACHERS HETASH SIR AND ANURAGE SIR. I LOVE THE WAY THEY EXPLAIN THE TOPICS.]
